f6572e1737332202bdad6e7b2a945d364f983ea4
mrchenshuai0-hue <mr.chenshuai0@gmail.com>
2026-04-10 17:40:04 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/福鼎市气象×环保智慧监管大屏/',
+    base: '/fuding/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
